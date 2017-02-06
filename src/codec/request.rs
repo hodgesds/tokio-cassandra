ca7841f29e43b8270cdaaf0728cfa3239cf64ff7
Nikolai <comavas@googlemail.com>
2017-02-06 08:47:28 +0100

    Further cleanup

--- src/codec/request.rs
+++ src/codec/request.rs
@@ -1,4 +1,3 @@
-use std::io;
 use codec::header::{OpCode, Header, ProtocolVersion, Direction};
 
 error_chain! {
@@ -16,16 +15,18 @@ error_chain! {
     }
 }
 
-pub trait CqlProtoEncode<W> {
+pub trait CqlEncode {
+    fn encode(&self, f: &mut Vec<u8>) -> Result<usize>;
+}
+
+pub trait Request {
     fn opcode() -> OpCode;
     fn protocol_version() -> ProtocolVersion;
-    fn encode(&self, f: &mut W) -> Result<usize>;
 }
 
 pub struct OptionsRequest;
-impl<W> CqlProtoEncode<W> for OptionsRequest
-    where W: io::Write
-{
+
+impl Request for OptionsRequest {
     fn opcode() -> OpCode {
         OpCode::Options
     }
@@ -33,37 +34,33 @@ impl<W> CqlProtoEncode<W> for OptionsRequest
     fn protocol_version() -> ProtocolVersion {
         ProtocolVersion::Version3(Direction::Request)
     }
+}
 
-    fn encode(&self, _: &mut W) -> Result<usize> {
+impl CqlEncode for OptionsRequest {
+    fn encode(&self, _: &mut Vec<u8>) -> Result<usize> {
         Ok(0)
     }
 }
 
-pub struct CqlEncoder;
-
-impl CqlEncoder {
-    pub fn encode<E>(&self, to_encode: E, body_buf: &mut Vec<u8>) -> Result<([u8; 9])>
-        where E: CqlProtoEncode<Vec<u8>>
-    {
-        let len = to_encode.encode(body_buf)?;
-        if len > u32::max_value() as usize {
-            return Err(ErrorKind::BodyLengthExceeded(len).into());
-        }
-        let len = len as u32;
-
-        let header = Header {
-            version: E::protocol_version(),
-            flags: 0x00,
-            stream_id: 270,
-            op_code: E::opcode(),
-            length: len,
-        };
-
-        let header_bytes = header.encode()?;
-
-        // TODO actual implementation
-        Ok(header_bytes)
+pub fn cql_encode<E>(to_encode: E, body_buf: &mut Vec<u8>) -> Result<([u8; 9])>
+    where E: Request + CqlEncode
+{
+    let len = to_encode.encode(body_buf)?;
+    if len > u32::max_value() as usize {
+        return Err(ErrorKind::BodyLengthExceeded(len).into());
     }
+    let len = len as u32;
+
+    let header = Header {
+        version: E::protocol_version(),
+        flags: 0x00, // TODO dont hardcode
+        stream_id: 270,
+        op_code: E::opcode(),
+        length: len,
+    };
+
+    let header_bytes = header.encode()?;
+    Ok(header_bytes)
 }
 
 
@@ -74,9 +71,8 @@ mod test {
     #[test]
     fn from_options_request() {
         let o = OptionsRequest;
-        let e = CqlEncoder;
         let mut buf = Vec::new();
-        let header_bytes = e.encode(o, &mut buf).unwrap();
+        let header_bytes = cql_encode(o, &mut buf).unwrap();
 
         let expected_bytes = b"\x03\x00\x01\x0e\x05\x00\x00\x00\x00";
 
