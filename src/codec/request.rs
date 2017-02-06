4d41e6c5d8a0159e58137f1273cba6491f65fa6b
Nikolai <comavas@googlemail.com>
2017-02-06 08:26:06 +0100

    Simplify encode interface

--- src/codec/request.rs
+++ src/codec/request.rs
@@ -42,13 +42,17 @@ impl<W> CqlProtoEncode<W> for OptionsRequest
 pub struct CqlEncoder;
 
 impl CqlEncoder {
-    pub fn encode<E, W, W2>(&self, to_encode: E, to_write_to: &mut W) -> Result<usize>
-        where E: CqlProtoEncode<W2>,
-              W2: io::Write + Default + AsRef<[u8]>,
+    pub fn encode<E, W>(&self,
+                        to_encode: E,
+                        to_write_to: &mut W,
+                        body_buf: &mut Vec<u8>)
+                        -> Result<usize>
+        where E: CqlProtoEncode<Vec<u8>>,
               W: io::Write
     {
-        let mut body = W2::default();
-        let len = to_encode.encode(&mut body)?;
+        assert!(body_buf.len() == 0,
+                "Can't handle non-empty body-bufs for now");
+        let len = to_encode.encode(body_buf)?;
         if len > u32::max_value() as usize {
             return Err(ErrorKind::BodyLengthExceeded(len).into());
         }
@@ -63,7 +67,7 @@ impl CqlEncoder {
         };
 
         header.encode(to_write_to)?;
-        to_write_to.write(&body.as_ref())?;
+        to_write_to.write(&body_buf.as_ref())?;
 
         Ok(9)
     }
@@ -79,7 +83,8 @@ mod test {
         let o = OptionsRequest;
         let e = CqlEncoder;
         let mut buf = Vec::new();
-        let len = e.encode::<_, _, Vec<_>>(o, &mut buf).unwrap();
+        let mut body_buf = Vec::new();
+        let len = e.encode(o, &mut buf, &mut body_buf).unwrap();
 
         let expected_bytes = b"\x03\x00\x01\x0e\x05\x00\x00\x00\x00";
 
