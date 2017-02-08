358e1adba91c59a32de8bc2317da53d5f14161da
Nikolai <comavas@googlemail.com>
2017-02-08 07:29:18 +0100

    Implemented encode and decode for string

--- src/codec/primitives.rs
+++ src/codec/primitives.rs
@@ -1,38 +1,55 @@
 pub mod decode {
-    use nom::{IResult, be_u16};
+    use nom::be_u16;
 
-    pub fn short(b: &[u8]) -> IResult<&[u8], u16> {
-        be_u16(b)
+    named!(pub short(&[u8]) -> u16, call!(be_u16));
+    named!(pub string(&[u8]) -> &str, do_parse!(
+            s: short          >>
+            str: take_str!(s) >>
+            (str)
+            )
+    );
+
+
+}
+
+pub mod encode {
+    use byteorder::{ByteOrder, BigEndian};
+
+    pub fn short(v: u16) -> [u8; 2] {
+        let mut bytes = [0u8; 2];
+        BigEndian::write_u16(&mut bytes[..], v);
+        bytes
     }
 
-    named!(string(u8) -> &str, take_str!(take!(u16!())));
+    pub fn string(s: &str) -> Vec<u8> {
+        let mut len = [0u8; 2];
+        BigEndian::write_u16(&mut len[..], s.len() as u16);
 
+        let mut buf: Vec<u8> = Vec::new();
+        buf.extend(&len[..]);
+        buf.extend(s.as_bytes());
+        buf
+    }
+}
 
-    #[cfg(test)]
-    mod test {
-        use byteorder::{ByteOrder, BigEndian};
-        use super::super::decode;
+#[cfg(test)]
+mod test {
+    use super::decode;
+    use super::encode;
 
-        #[test]
-        fn short() {
-            let mut buf = [0u8; 2];
-            let expected = 342;
-            BigEndian::write_u16(&mut buf[..], expected);
+    #[test]
+    fn short() {
+        let expected: u16 = 342;
+        let buf = encode::short(expected);
 
-            assert_finished_and_eq!(decode::short(&buf), expected);
-        }
+        assert_finished_and_eq!(decode::short(&buf), expected);
+    }
 
-        #[test]
-        fn string() {
-            let s = "Hello üß";
-            let mut len = [0u8; 2];
-            BigEndian::write_u16(&mut len[..], s.len() as u16);
+    #[test]
+    fn string() {
+        let s = "Hello üß";
+        let buf = encode::string(s);
 
-            let buf = Vec::new();
-            buf.extend(len);
-            buf.extend(s.as_bytes());
-
-            assert_finished_and_eq!(decode::string(&buf), s);
-        }
+        assert_finished_and_eq!(decode::string(&buf), s);
     }
 }
