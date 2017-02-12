4a34e3e7aa351aa7295626305dc64683db8eebb4
Sebastian Thiel <byronimo@gmail.com>
2017-02-12 11:42:46 +0100

    indexed string decoding

--- src/codec/primitives/decode_indexed.rs
+++ src/codec/primitives/decode_indexed.rs
@@ -33,14 +33,28 @@ pub fn short(i: &[u8]) -> ParseResult<u16> {
     Ok((&i[2..], res))
 }
 
+pub fn string(ofs: usize, i: &[u8]) -> ParseResult<indexed::CqlString> {
+    let (i, s) = short(i)?;
+    if i.len() < s as usize {
+        return Err(Incomplete(Size(s as usize)));
+    }
+    Ok((&i[s as usize..],
+        indexed::CqlString {
+            at: ofs + 2,
+            len: s,
+        }))
+}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
+    use super::super::{indexed, encode, borrowed};
     use byteorder::{ByteOrder, BigEndian};
 
     # [test]
     fn short_incomplete() {
-        assert_eq!(short(&[0]), Err(Error::Incomplete(Needed::Size(2))));
+        assert_eq!(short(&[0]), Err(Incomplete(Size(2))));
     }
 
     #[test]
@@ -49,5 +63,23 @@ mod test {
         let expected = 16723;
         BigEndian::write_u16(&mut b, expected);
         assert_eq!(short(&b[..]), Ok((&b[2..], expected)));
+    }
+
+    #[test]
+    fn string_complete() {
+        let s = borrowed::CqlString::try_from("hello").unwrap();
+        let ofs = 5;
+        let mut b: Vec<_> = (0u8..5).collect();
+        encode::string(&s, &mut b);
+        b.extend(0..2);
+
+        assert_eq!(string(ofs, &b[ofs..ofs + 1]), Err(Incomplete(Size(2))));
+        assert_eq!(string(ofs, &b[ofs..ofs + 4]), Err(Incomplete(Size(5))));
+        assert_eq!(string(ofs, &b[ofs..]),
+                   Ok((&b[ofs + 2 + 5..],
+                       indexed::CqlString {
+                           at: ofs + 2,
+                           len: 5,
+                       })));
     }
 }
