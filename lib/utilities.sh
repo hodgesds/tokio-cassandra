23793c253114e7c6dd6230b0ddae191d5790b321
Nikolai <comavas@googlemail.com>
2017-02-16 16:54:10 +0100

    Docker run -d cannot be used in conjuction with --rm

--- lib/utilities.sh
+++ lib/utilities.sh
@@ -7,7 +7,7 @@ start_dependencies() {
 	CASSANDRA_PORT=${1:?Need cassandra port to expose/expect on host}
 	TESTER=${2:?Need command line to execute to see if cassandra is up}
 	echo starting dependencies
-	docker run --rm --name "$CONTAINER_NAME" -d -p "$CASSANDRA_HOST":"$CASSANDRA_PORT":9042 --expose 9042 cassandra:2.1 1>&2
+	docker run --name "$CONTAINER_NAME" -d -p "$CASSANDRA_HOST":"$CASSANDRA_PORT":9042 --expose 9042 cassandra:2.1 1>&2
 	while ! $TESTER "$CASSANDRA_HOST" "$CASSANDRA_PORT"; do
 		echo "Waiting for cassandra on $CASSANDRA_HOST:$CASSANDRA_PORT" 1>&2
 		sleep 1
