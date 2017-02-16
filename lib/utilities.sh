a8425170fd4629fcf61e673631aa0da4af7f2eab
Nikolai <comavas@googlemail.com>
2017-02-16 16:47:02 +0100

    fixed bug in shell file

--- lib/utilities.sh
+++ lib/utilities.sh
@@ -3,21 +3,19 @@
 CONTAINER_NAME=db
 CASSANDRA_HOST=127.0.0.1
 
-function start_dependencies() {
-    local CASSANDRA_PORT=${1:?Need cassandra port to expose/expect on host}
-    local TESTER=${2:?Need command line to execute to see if cassandra is up}
-    echo starting dependencies
-    docker run --rm --name $CONTAINER_NAME -d -p $CASSANDRA_HOST:$CASSANDRA_PORT:9042 --expose 9042 cassandra:2.1 1>&2
-    while ! $TESTER $CASSANDRA_HOST $CASSANDRA_PORT; do
-        echo "Waiting for cassandra on $CASSANDRA_HOST:$CASSANDRA_PORT" 1>&2
-        sleep 1
-    done
-    echo "Cassandra up on $CASSANDRA_HOST:$CASSANDRA_PORT" 1>&2
+start_dependencies() {
+	CASSANDRA_PORT=${1:?Need cassandra port to expose/expect on host}
+	TESTER=${2:?Need command line to execute to see if cassandra is up}
+	echo starting dependencies
+	docker run --rm --name "$CONTAINER_NAME" -d -p "$CASSANDRA_HOST":"$CASSANDRA_PORT":9042 --expose 9042 cassandra:2.1 1>&2
+	while ! $TESTER "$CASSANDRA_HOST" "$CASSANDRA_PORT"; do
+		echo "Waiting for cassandra on $CASSANDRA_HOST:$CASSANDRA_PORT" 1>&2
+		sleep 1
+	done
+	echo "Cassandra up on $CASSANDRA_HOST:$CASSANDRA_PORT" 1>&2
 }
 
-function stop_dependencies() {
-    if [ -z "$TRAVIS" ]; then
-        echo stopping dependencies ...
-        docker rm --force $CONTAINER_NAME >/dev/null
-    fi
+stop_dependencies() {
+	echo stopping dependencies ...
+	docker rm --force $CONTAINER_NAME >/dev/null
 }
