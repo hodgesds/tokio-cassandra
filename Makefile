8fa4fbd4f77c0312c8866ddbed0b4e989107478c
Sebastian Thiel <byronimo@gmail.com>
2017-03-02 09:07:08 +0100

    Add preliminary support for tls and certificates in CLI

--- Makefile
+++ Makefile
@@ -47,7 +47,7 @@ attach-docker-db:
 	DEBUG_RUN_IMAGE=true $(MAKE) $(type)-docker-db
 
 cli-tests:
-	cd cli && cargo run --all-features -- test-connection -u cassandra -p cassandra 127.0.0.1 9042
+	cd cli && cargo run --all-features -- test-connection --tls --cert-type pk12 --cert ./etc/secrets/docker-cassandra/secrets/keystore.pk12:cassandra 127.0.0.1 9042
 
 secrets:
 	$(MAKE) -C etc/docker-cassandra $@
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -19,8 +19,16 @@ use tcc::errors::*;
 
 quick_main!(run);
 
+arg_enum!{
+    #[derive(Debug)]
+    pub enum CertKind {
+        PK12
+    }
+}
+
 pub fn run() -> Result<()> {
     env_logger::init().unwrap();
+    let default_cert_type = format!("{}", CertKind::PK12);
 
     let mut app: clap::App = app_from_crate!();
     app = app.subcommand(SubCommand::with_name("test-connection")
@@ -37,12 +45,35 @@ pub fn run() -> Result<()> {
             .required(false)
             .short("u")
             .long("user")
-            .takes_value(true))
+            .takes_value(true)
+            .help("The name of the user to login authenticate as"))
         .arg(Arg::with_name("password")
             .required(false)
             .short("p")
             .long("password")
-            .takes_value(true)));
+            .takes_value(true)
+            .help("The user's password. Please note that the password might persist in your \
+                   history file if provided here"))
+        .arg(Arg::with_name("tls")
+            .required(false)
+            .takes_value(false)
+            .long("tls")
+            .help("Encrypt the connection via TLS. This will never connect via plain-text, \
+                   even if the server supports that too."))
+        .arg(Arg::with_name("cert-type")
+            .required(false)
+            .takes_value(true)
+            .long("cert-type")
+            .default_value(&default_cert_type)
+            .help("Encrypt the connection via TLS. This will never connect via plain-text, \
+                   even if the server supports that too."))
+        .arg(Arg::with_name("cert")
+            .required(false)
+            .takes_value(true)
+            .long("cert")
+            .help("The path to the certificate file in a format defined by --cert-type. A \
+                   password can be provided by separating it with a colon, such as in \
+                   /path/to/cert:password.")));
     let args: clap::ArgMatches = app.get_matches();
     match args.subcommand() {
         ("test-connection", Some(args)) => tcc::test_connection(args),
